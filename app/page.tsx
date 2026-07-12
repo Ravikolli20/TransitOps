b6ec2ef1e6a071a9c92763c751d150f3b5d5b067
Ravi Kolli <150434508+Ravikolli20@users.noreply.github.com>
2026-07-12 12:53:27 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getAccessToken } from '@/lib/session';
-
-export default function RootPage() {
-  redirect(getAccessToken() ? '/dashboard' : '/login');
-}
